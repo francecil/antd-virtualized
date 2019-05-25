92d9e4235e2f3f78cb44a8d8911820d4eaa4a62b
Francecil <396324491@qq.com>
2019-05-25 11:08:44 +0800

    未封装，初步完成大数据Select内容的显示

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
-
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+import { Button } from 'antd';
+import styles from './App.module.css'
+import Select from './Select'
+const initialState = {
+  filtersSerial: undefined,
+  options: Array.from({ length: 2000 }).map((v, i) => ({ id: i, name: 'test' + i }))
+};
+type IState = Readonly<typeof initialState>;
+class App extends React.Component<{}, IState> {
+  // handleChange = (v: any, type: string) => {
+  //   console.log(v, type)
+  // }
+  readonly state: IState = initialState;
+  render() {
+    const { filtersSerial,options } = this.state
+    return (
+      <div className={styles.App}>
+        <header className={styles.AppHeader}>
+          <img src={logo} className={styles.AppLogo} alt="logo" />
+          <Button type="primary">Button</Button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React start
+          <Select
+            allowClear
+            placeholder="请输入，并选择"
+            className={styles.select}
+            labelKey='name'
+            onChange={(v: any) => console.log(v)}
+            options={options}
+            showSearch
+            value={filtersSerial}
+            valueKey='id'
+          // filterOption={(input, option) => option.serialNo.indexOf(input) >= 0}
+          />
+          <a
+            className={styles.AppLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React start
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/index.d.ts
@@ -0,0 +1,2 @@
+declare module 'react-virtualized/dist/commonjs/AutoSizer';
+declare module 'react-virtualized/dist/commonjs/List';
