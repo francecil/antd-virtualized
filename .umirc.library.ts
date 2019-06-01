35cabe5bce566f08d1f776d6d9986205772fc4cf
Francecil <396324491@qq.com>
2019-06-01 11:42:56 +0800

    docz 搭建完毕

--- .umirc.library.ts
+++ .umirc.library.ts
@@ -1,9 +1,38 @@
 import { IBundleOptions } from 'umi-library/src/types';
-
+//note:需要重新run dev 该文件修改才会生效
 const options: IBundleOptions = {
   cjs: 'rollup',
   esm: 'rollup',
-  doc: { typescript: true },
+  doc: {
+    themeConfig: {
+      title: 'Antd Virtualized',
+      colors: {
+        primary: '#bd4932',
+        link: '#bd4932',
+      },
+    },
+    typescript: true,
+    menu: [
+      {
+        name: '介绍'
+      },
+      {
+        name: '快速开始'
+      },
+      {
+        name: '脚手架'
+      },
+      {
+        name: '组件规范'
+      },
+      {
+        name: 'FAQ'
+      },
+      {
+        name: 'Components'
+      },
+    ]
+  },
 };
 
 export default options;
